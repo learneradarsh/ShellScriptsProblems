bb4590ffe454f20e1b6a334e38b6b5017be8e7eb
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-09 19:15:02 +0530

    REMOVED COMMAND LINE INPUT
    Implemented command line input and removed user input

--- dayFinder.sh
+++ dayFinder.sh
@@ -3,12 +3,10 @@
 #day finder using Gregorian Calendar Formula by taking date as a input
 
 function findDayFromDate(){
-	echo $month $day $year
-	yZero=$(($year-$(($((14-$month))/12))))
+	yZero=$(($3-$(($((14-$1))/12))))
 	x=$(($yZero+$(($yZero/4))-$(($yZero/100))+$(($yZero/400))))
-	mZero=$(($month+12*$(($(($((14-$month))/12))-2))))
-	day=$(($(($day+$x+31*$mZero/12))%7))
+	mZero=$(($1+12*$(($(($((14-$1))/12))-2))))
+	day=$(($(($2+$x+31*$mZero/12))%7))
 	echo $day
 }
-read -p "Enter date  in m(month) d(day) y(year) format:" month day year
-findDayFromDate $month $day $year
+findDayFromDate $1 $2 $3
