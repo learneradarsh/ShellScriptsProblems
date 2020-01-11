1a83f51a9f2fb27023b850a2e8d1c13855341d87
learneradarsh <learner.adarsh@gmail.com>
2020-01-11 18:22:48 +0530

    ADDED arrayFour.sh
    Implemented function to find triplet of zero using array and sort method of shell

--- arrayFour.sh
+++ arrayFour.sh
@@ -1,10 +1,47 @@
 #!/bin/bash
 
 num=() #initiliased array
-i=0
+k=0
+readonly SUM=0
+size=0
 
-read -p "Number of inputs:" input
-while (( i<input ))
+#function for asc sorting
+function ascSort(){
+	num=($(echo ${num[*]}| tr " " "\n" | sort -n))
+}
+
+read -p "Total Number of inputs:" size
+echo  "Enter numbers:"
+while (( $k<$size ))
 do
-	num[{}]
+	read num[$k]
+	(( k++ ))
 done
+
+
+#function to find triplet of zero
+function findTriplet(){
+	ascSort ${num[@]}
+	while (( x<size-2 ))
+	do
+		l=$(( x+1 ))
+		r=$(( size-1 ))
+		while (( l<r ))
+		do
+			if (( ${num[$i]}+${num[$l]}+${num[$r]}==SUM ))
+			then
+				echo "Triplet is:" ${num[$i]} ${num[$l]} ${num[$r]}
+				return 1
+			elif (( ${num[$i]}+${num[$l]}+${num[$r]}<SUM ))
+			then
+				(( l++ ))
+			else
+				(( r-- ))
+			fi
+		done
+		(( x++ ))
+	done
+	echo "No Triplet Found"
+}
+
+findTriplet
