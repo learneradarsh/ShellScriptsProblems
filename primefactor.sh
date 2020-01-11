6cf3100ebc32206e7caf5c965fb6d7a3158df03f
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-11 12:58:59 +0530

    REFACTORED : Rectified logic issue for prime factor function

--- primefactor.sh
+++ primefactor.sh
@@ -1,9 +1,24 @@
-#!/bin/bash -x
+#!/bin/bash
 
 function printPrimefact(){
-	for (( i=0 ; $(i*i) < num ; i++ ))
+	n=$1
+	while (( n%2 == 0 ))
 	do
-		echo $i
+		echo "2"
+		n=$(( n/2 ))
 	done
+	for (( i=3 ; $(( i*i ))<=n ; i=1+2 ))
+	do
+		while (( n%$i==0 ))
+		do
+			echo $i
+			n=$(( n/$i ))
+		done
+	done
+	if (( n>2 ))
+	then
+		echo $n
+	fi
 }
 read -p "Enter number:" num
+printPrimefact $num
