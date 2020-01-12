8992c97357166d5ef7c4479ad0f74c681efb0044
learneradarsh <learner.adarsh@gmail.com>
2020-01-12 15:33:22 +0530

    ADDED smaller and greater functions
    Added smaller and greater functions to calculate magical number as per user input

--- magicNumber.sh
+++ magicNumber.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-i=0
-echo "Think of a Number between 1 to 100:"
-while [0]
-do
- echo ""
-done
-read -p "Number is less than 50(Y/N):" ans
+readonly YES="Y"
+readonly NO="N"
 
+echo "Think of a Number between 1 to 100:"
+
+function greater(){
+	echo "greater"
+}
+
+function smaller(){
+	for (( i=50 ; i<50 ; i++ ))
+	do
+		return 
+	done
+}
+
+while :
+do
+read -p "Number is less than 50(Y/N):" ans
+if (( "$ans" == "$YES" ))
+then
+	smaller
+elif (( "$ans" == "$NO" ))
+then
+	greater
+else
+	echo "Not a valid input"
+fi
+done
