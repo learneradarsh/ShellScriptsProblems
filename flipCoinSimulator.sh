8add11e67f6285b2797de950aa28f56ae2763ac6
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-13 18:34:16 +0530

    MODIFIED: ADDED USECASE 4
    Implemented logic for finding triplet combination and printing percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -7,6 +7,7 @@
 #DICTIONARY
 declare -a singlet=( [0]=0 [1]=0 )
 declare -a doublet=( [00]=0 [01]=0 [11]=0 [10]=0 )
+declare -a triplet=( [000]=0 [001]=0 [011]=0 [111]=0 [101]=0 [110]=0 [100]=0 [010]=0 )
 
 #VARIABLES
 multiple=0
@@ -25,6 +26,13 @@ function getDoubletRandom(){
 	echo $randNumber
 }
 
+function getTripletRandom(){
+	local r1=$((RANDOM%2))
+	local r2=$((RANDOM%2))
+	local r3=$((RANDOM%2))
+	local randNumber="${r1}${r2}${r3}"
+	echo $randNumber
+}
 while (( i<$multiple ))
 do
 	r=$(getSingletRandom)
@@ -57,6 +65,36 @@ do
 		echo "Nothing"
 		;;
 	esac
+	r=$(getTripletRandom)
+	case $r in
+		000)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		001)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		011)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		111)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		100)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		110)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		010)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		101)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		*)
+		echo "Nothing"
+		;;
+	esac
 	(( i++ ))
 done
 
@@ -84,3 +122,36 @@ echo "HT %:" $res
 echo "Doublet TH: " ${doublet[01]}
 res=`echo "scale=3; ${doublet[01]}*100/$multiple" | bc`
 echo "TH %:" $res
+
+#triplet
+echo "Triplet HHH: " ${triplet[111]}
+res=`echo "scale=3; ${triplet[111]*100/$multiple}" | bc`
+echo "HHH %:" $res
+
+echo "Triplet TTT: " ${triplet[000]}
+res=`echo "scale=3; ${triplet[000]*100/$multiple}" | bc`
+echo "TTT %:" $res
+
+echo "Triplet HTT: " ${triplet[100]}
+res=`echo "scale=3; ${triplet[100]*100/$multiple}" | bc`
+echo "HTT %:" $res
+
+echo "Triplet HHT: " ${triplet[110]}
+res=`echo "scale=3; ${triplet[110]*100/$multiple}" | bc`
+echo "HHT %:" $res
+
+echo "Triplet HTH: " ${triplet[101]}
+res=`echo "scale=3; ${triplet[101]*100/$multiple}" | bc`
+echo "HTH %:" $res
+
+echo "Triplet THT : " ${triplet[010]}
+res=`echo "scale=3; ${triplet[010]*100/$multiple}" | bc`
+echo "THT %:" $res
+
+echo "Triplet THH: " ${triplet[011]}
+res=`echo "scale=3; ${triplet[011]*100/$multiple}" | bc`
+echo "THH %:" $res
+
+echo "Triplet TTH: " ${triplet[001]}
+res=`echo "scale=3; ${triplet[00]*100/$multiple}" | bc`
+echo "TTH %:" $res
