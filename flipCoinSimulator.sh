727916700947170cc4e232efb13f528f9197e5f8
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-13 17:46:21 +0530

    MODIFIED: ADDED USECASE 2
    Implemented logic to store coin flip value using dictionary.
    And printed dictionary with Heads and Tails count percentage.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,16 +1,45 @@
 #!/bin/bash
 
+#ASSUMPTIONS
+#1=HEADS
+#0=TAILS
+
+#DICTIONARY
+declare -a singlet=( [0]=0 [1]=0 )
+
 #VARIABLES
+counter=0
+multiple=0
+
+read -p "Enter number:" multiple
 
 function getSingletRandom(){
 	local randNumber=$((RANDOM%2))
-	if (( randNumber==1 ))
-	then
-		echo "HEADS"
-	else
-		echo "TAILS"
-	fi
-	#echo $randNumber
+	echo $randNumber
 }
 
-getSingletRandom
+while (( i<$multiple ))
+do
+	r=$(getSingletRandom)
+	case $r in
+		1)
+		singlet[$r]=$(( ${singlet[$r]}+1 ))
+		;;
+		0)
+		singlet[$r]=$(( ${singlet[$r]}+1 ))
+		;;
+		*)
+		echo "Nothing"
+		;;
+	esac
+	(( i++ ))
+done
+
+echo "Singlet HeadsC: " ${singlet[1]}
+res=`echo "scale=3; ${singlet[1]}*100/$multiple" | bc`
+echo "HEADS %:" $res
+echo "Singlet TailsC: " ${singlet[0]}
+res=`echo "scale=3; ${singlet[0]}*100/$multiple" | bc`
+echo "TAILS %:" $res
+
+
