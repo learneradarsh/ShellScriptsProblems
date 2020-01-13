03e93c8e0beb400900484341e5e6dabdcc540167
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-13 18:08:46 +0530

    MODIFIED: ADDED USECASE 3
    1. Created function getDoubletRandom to get 2 bit random binary values
    2. Declared doublet dictionary to store HH,TH,TT,HT counts
    3. Implemented logic to store results in dictionary and print percentage of HH,TT,TH, and HT

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -6,15 +6,22 @@
 
 #DICTIONARY
 declare -a singlet=( [0]=0 [1]=0 )
+declare -a doublet=( [00]=0 [01]=0 [11]=0 [10]=0 )
 
 #VARIABLES
-counter=0
 multiple=0
 
 read -p "Enter number:" multiple
 
 function getSingletRandom(){
 	local randNumber=$((RANDOM%2))
+	echo $randNumber
+}
+
+function getDoubletRandom(){
+	local r1=$((RANDOM%2))
+	local r2=$((RANDOM%2))
+	local randNumber="${r1}${r2}"
 	echo $randNumber
 }
 
@@ -32,9 +39,28 @@ do
 		echo "Nothing"
 		;;
 	esac
+	r=$(getDoubletRandom)
+	case $r in
+		00)
+		doublet[$r]=$(( ${doublet[$r]}+1 ))
+		;;
+		01)
+		doublet[$r]=$(( ${doublet[$r]}+1 ))
+		;;
+		10)
+		doublet[$r]=$(( ${doublet[$r]}+1 ))
+		;;
+		11)
+		doublet[$r]=$(( ${doublet[$r]}+1 ))
+		;;
+		*)
+		echo "Nothing"
+		;;
+	esac
 	(( i++ ))
 done
 
+#singlet
 echo "Singlet HeadsC: " ${singlet[1]}
 res=`echo "scale=3; ${singlet[1]}*100/$multiple" | bc`
 echo "HEADS %:" $res
@@ -42,4 +68,19 @@ echo "Singlet TailsC: " ${singlet[0]}
 res=`echo "scale=3; ${singlet[0]}*100/$multiple" | bc`
 echo "TAILS %:" $res
 
+#doublet
+echo "Doublet TT: " ${doublet[00]}
+res=`echo "scale=3; ${doublet[00]}*100/$multiple" | bc`
+echo "TT %:" $res
 
+echo "Doublet HH: " ${doublet[11]}
+res=`echo "scale=3; ${doublet[11]}*100/$multiple" | bc`
+echo "HH %:" $res
+
+echo "Doublet HT: " ${doublet[10]}
+res=`echo "scale=3; ${doublet[10]}*100/$multiple" | bc`
+echo "HT %:" $res
+
+echo "Doublet TH: " ${doublet[01]}
+res=`echo "scale=3; ${doublet[01]}*100/$multiple" | bc`
+echo "TH %:" $res
