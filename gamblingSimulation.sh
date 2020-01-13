3dd06de3cce1afc2292eb2d62146e9837b52b640
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-13 19:38:44 +0530

    MODIFIED

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,9 +1,37 @@
-#!/bin/bash
+#!/bin/bash -x
 
-GAMBLEAMOUNT=100 #total gamble amount
-TOTALAMTWON=0 #total amount won
-TOTALAMTLOST=0
-DAYSWON=0
-DAYSLOST=0
-WONMAX=0
-LOSTMAX=0
+#CONSTANTS
+readonly WIN=1
+readonly LOSE=0
+
+#VARIABLES
+gambleAmount=100 #total gamble amount
+totalAmtWon=0 #total amount won
+totalAmtLost=0
+daysWon=0
+daysLost=0
+wonMax=0
+lostMax=0
+
+i=1
+
+function gambler(){
+	bet=$((RANDOM%2))
+	if (( $bet==1 ))
+	then
+		(( gambleAmount++ ))
+		return 1
+	else
+		(( gambleAmount-- ))
+		return 0
+	fi
+}
+
+while(( i<=$gambleAmount ))
+do
+	gambler
+	if (( $gamebleAmount==0 ))
+	then
+		echo "you lost"
+	(( i++ ))
+done
