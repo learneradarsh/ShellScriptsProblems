374214aa0496c4699e8a08b6aeca879fdeb8f7a8
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-14 16:08:35 +0530

    MODIFIED: ADDED USECASE 1
    1. Printed instructions.
    2. Declared arrays for snake and ladders position

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,3 +1,36 @@
-#!/bin/bash
+#!/bin/bash -x
+
+#CONSTANTS
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+#ARRAYS
+declare -a SNAKEH=(38 51 76 91 97)
+declare -a SNAKET=(20 10 54 73 61)
+declare -a LADDERT=(5 14 53 64)
+declare -a LADDERH=(58 49 72 83)
+
 
 echo "WELCOME TO SNAKE LADDER GAME"
+echo "========LADDER=========="
+echo "5-58"
+echo "14-49"
+echo "53-72"
+echo "64-83"
+
+echo "=======SNAKES========"
+echo "38-20"
+echo "51-10"
+echo "76-54"
+echo "91-73"
+echo "97-61"
+
+function printBoard(){
+	echo "=========================GAMEBOARD==============================="
+}
+
+printBoard
+
+echo "Game Starts With Zero:"
+echo "Player 1: 0"
+echo "Player 2: 0"
