e22fffd5e10edd98d2226185b457a2d8586e8968
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-14 12:16:40 +0530

    MODIFIED: Rectified Logic for USECASE 4
    Change comparsion conditions for gambleAmount

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -7,17 +7,15 @@ readonly MAXWON=150 #maximum won amount limit
 readonly RESAMT=50 #resign amount
 
 #VARIABLES
-gambleAmount=100 #total gamble amount
 totalAmtWon=0 #total amount won
 totalAmtLost=0
 daysWon=0
 daysLost=0
 dayCounter=1
-i=1
+betCounter=1
 
 function gamble(){
-	chance=$((RANDOM%2))
-	if (( chance==WIN ))
+	if (( $chance==$WIN ))
 	then
 		return 1
 	else
@@ -25,29 +23,29 @@ function gamble(){
 	fi
 }
 
-for (( dayCounter=1 ; $dayCounter<=20 ; dayCounter++ ))
+for (( dayCounter=1 ; dayCounter<=20 ; dayCounter++ ))
 do
-	while (( i<=$gambleAmount ))
+	gambleAmount=100 #total gamble amount
+	while (( ($gambleAmount<$MAXWON) && ($gambleAmount>$RESAMT) ))
 	do
-		gamble
-		if (( $?==$WIN ))
+		chance=$((RANDOM%2))
+		if (( $chance==$WIN ))
 		then
-			(( ++gambleAmount ))
-		elif (( $gambleAmount==$MAXWON ))
-		then
-			echo "YOU WON.AMOUNT IS:" $gambleAmount
-			totalAmtWon=$(( $totalAmtWon+$gambleAmount ))
-			break
-		elif (( $gambleAmount==$RESAMT ))
-		then
-			echo "YOU LOSE.PENDING MONEY IS:" $gambleAmount
-			totalAmtLost=$(( $totalAmtLost+$gambleAmount ))
-			break
+			(( gambleAmount++ ))
 		else
-			(( --gambleAmount ))
+			(( gambleAmount-- ))
 		fi
-		(( i++ ))
 	done
+	if ((gambleAmount==RESAMT))
+	then
+		echo "YOU LOST: AMOUNT IS:" $gambleAmount
+		totalAmtLost=$(( totalAmtLost+gambleAmount ))
+		echo $totalAmtLost
+	else
+		echo "YOU WON: AMOUNT IS:" $gambleAmount
+		totalAmtWon=$(( totalAmtWon+gambleAmount ))
+		echo $totalAmtWon
+	fi
 done
-echo $totalAmtWon
-echo $totalAmtLost
+echo "total amt won:" $totalAmtWon
+echo "total amt lost:" $totalAmtLost
