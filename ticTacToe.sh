2729dc1a6f64b9f2181c5cc749a1434631c5d8f7
learneradarsh <learner.adarsh@gmail.com>
2020-01-15 02:25:36 +0530

    MODIFIED: ADDED USECASE 1
    Implemented function printBoard() to print board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "WELCOME TO TIC TAC TOE"
+echo "======================"
+
+#VARIABLES
+board=(. . . . . . . . .)
+
+function printBoard(){
+  echo "r\c 0 1 2"
+  echo "0   ${board[0]} ${board[1]} ${board[2]}"
+  echo "1   ${board[3]} ${board[4]} ${board[5]}"
+  echo "2   ${board[6]} ${board[7]} ${boad[8]}"
+}
+
+printBoard
