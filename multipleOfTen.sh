3bf0a1283cb59a9ac250ff55c96d79003af2fb71
learneradarsh <learner.adarsh@gmail.com>
2020-01-16 23:46:12 +0530

    MODIFIED: LOGIC MODIFIED
    Coevered range of digits for ones, tens, hundred etc.

--- multipleOfTen.sh
+++ multipleOfTen.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
 
 function convertNumberInWord(){
-	if (( number == 1))
+	if (( number >= 0 )) && (( number <= 9 ))
 	then
-		echo "One"
-	elif (( number == 10 ))
-	then
-		echo "Ten"
-	elif (( number == 100 ))
+		echo "Ones"
+	elif (( number >= 10 )) && (( number <= 99 ))
+ 	then
+		echo "Tens"
+	elif (( number >= 100 )) && (( number <= 999 ))
 	then
 		echo "Hundred"
-	elif (( number == 1000 ))
+	elif (( number >= 1000 )) && (( number <= 9999 ))
 	then
 		echo "Thousand"
-	elif (( number == 10000 ))
+	elif (( number == 10000 )) && (( number <= 99999 ))
 	then
 		echo "Ten Thousand"
-	elif (( number == 100000 ))
+	elif (( number == 100000 )) && (( number <= 999999 ))
 	then
 		echo "Lakh"
-	elif (( number == 1000000 ))
-	then
-		echo "Ten Lakh"
-	elif (( number == 10000000 ))
-	then
-		echo "Crore"
-	elif (( number == 100000000 ))
-	then
-		echo "Ten Crore"
 	else
 		echo "Out of Limit"
 	fi
 }
-read -p "Enter a number in multiple of 10:" number
+read -p "Enter a number" number
 convertNumberInWord number
