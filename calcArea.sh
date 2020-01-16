7bed74f2520489737011031b73a82e792c5b699c
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-16 19:01:39 +0530

    MODIFIED: FORMATTED CODE

--- calcArea.sh
+++ calcArea.sh
@@ -4,13 +4,16 @@ function convertFeetIntoMeters(){
 	resInMeters=`echo "scale=2; $1/3.2808" | bc`
 	echo $resInMeters
 }
+
 function convertMeterIntoAcres(){
 	resInAcres=`echo "scale=2; $1*0.00024711" | bc`
 	echo $resInAcres
 }
+
 function areaOfPlot(){
 	echo $(($1*$2))
 }
+
 finalAreaOfPlots=$(($(areaOfPlot 60 40)*25))
 finalAreaInMeters=$(convertFeetIntoMeters $finalAreaOfPlots)
 finalAreaInAcres=$(convertMeterIntoAcres $finalAreaInMeters)
